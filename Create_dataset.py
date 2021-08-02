c6a791ea00a2890471d829eeba30eb4ce0d42f59
matte <aRx58Sn?>
2021-08-02 12:01:03 +0200

    -make Main.py script and define Create_dataset.py

--- Create_dataset.py
+++ Create_dataset.py
@@ -1,11 +1,14 @@
 import math
-
+import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-
 from pycocotools.coco import COCO
 import json
-#import numpy as np
+
+def cut_strings(s):
+    words = s.split(' ')
+    split = int(math.ceil(len(words)/2.0))
+    return " ".join(words[:split])," ".join(words[split:])
+
 
 # initialize COCO api for caption annotations
 train= {}
@@ -20,11 +23,16 @@ for x in anns_train:
     train[x['id']] = {"cap" : cap, "im_id" : im_id}
 
 
-
+#saving all categories and supercategories [category:supercategory]
 test_im_cat = "./annotations-test/image_info_test2014.json"
 file = open(test_im_cat,"r")
 j_data = json.load(file)
-#print(j_data['images']['id'][2])
+
+categories = {}
+for item in j_data['categories']:
+ categories[item['name']] = item['supercategory']
+print(categories)
+
 
 # initialize COCO api for caption annotations
 val={}
@@ -39,62 +47,26 @@ for x in anns:
 
 print('val: '+str(len(val)))
 print('train: '+str(len(train)))
-lista_tot= {**train, **val}
-print('somma: '+str(len(lista_tot)))
+training_data = {**train, **val}
+print('somma: '+str(len(training_data)))
 
-def cut_strings(s):
-    words = s.split(' ')
-    split = int(math.ceil(len(words)/2.0))
-    return " ".join(words[:split])," ".join(words[split:])
+train_file = open("train.txt", "w")
+i=0
+for idx in training_data.keys():
+    train_file.write(training_data[idx]['cap']+'\n')
+    i=i+1
+    if i==40000:
+        break
+train_file.close()
+print(i)
 
 
-train_ratio = 0.8
-validation_ratio = 0.1
-test_ratio = 0.1
-
-df = pd.DataFrame(lista_tot)
-training_data = df.sample(frac=train_ratio, random_state=25)
-testing_data = df.drop(training_data.index)
-
-print('train: '+str(len(training_data)))
-print('test: '+str(len(testing_data)))
-
-test_data = testing_data.values.tolist()
-train_data = training_data.values.tolist()
-
-all_x = []
-all_y = []
-for s in test_data:
-    first_half, second_half = cut_strings(s[0])  #poichè test data è una lista di liste bisogna fare accesso alla lista interna
-    all_x.append(first_half)
-    all_y.append(second_half)
-
-X = all_x
-Y = all_y
-
-x_val, x_test, y_val, y_test = train_test_split(X,Y, test_size=test_ratio/(test_ratio + validation_ratio))
-
-a_file = open("train.txt", "w")
-for row in train_data:
-    a_file.write(row[0]+'\n')
-a_file.close()
-
-a_file = open("x_val.txt", "w")
-for row in x_val:
-    a_file.write(row+'\n')
-a_file.close()
-
-a_file = open("y_val.txt", "w")
-for row in y_val:
-    a_file.write(row + '\n')
-a_file.close()
-
-a_file = open("x_test.txt", "w")
-for row in x_test:
-    a_file.write(row+'\n')
-a_file.close()
-
-a_file = open("y_test.txt", "w")
-for row in y_test:
-    a_file.write(row+'\n')
-a_file.close()
+#create seed and reference file
+seed_id = list(training_data.keys())[40000:41000]
+seed_file = open("seed_file.txt", "w")
+reference_file = open('reference.txt','w')
+for id in seed_id:
+    first_half, second_half = cut_strings(training_data[id]['cap'])
+    reference_file.write(training_data[id]['cap']+'\n')
+    seed_file.write(first_half+'\n')
+seed_file.close()
---
+++ Main.py
@@ -0,0 +1,23 @@
+import os
+
+from ipython_genutils.py3compat import execfile
+
+os.system('unzip ./Dataset/coco_annotation.zip -d ./Dataset')
+os.system('unzip ./annotations-test/image_info_test2014.zip -d ./annotations-test')
+
+os.system('python Create_dataset.py')
+
+os.system('git clone https://github.com/salesforce/ctrl.git')
+
+os.system('pip install fastBPE')
+
+os.system('python ./ctrl/training_utils/make_tf_records.py --text_file train.txt --control_code Caption --sequence_len 256')
+
+os.system('gsutil -m cp -r gs://sf-ctrl/seqlen256_v1.ckpt/ .')
+
+os.system('python ./ctrl/training_utils/training.py --model_dir ./ctrl/seqlen256_v1.ckpt --iterations 500')
+
+os.system(' python ./ctrl/generation.py --model_dir ./ctrl/seqlen256_v1.ckpt --temperature 0.4 --topk 4')
+
+
+
