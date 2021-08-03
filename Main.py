49a8db5131c8e070798638a38fb7bc7b045f8ee0
swearato <s277979@studenti.polito.it>
2021-08-03 11:17:27 +0200

    fixed main

--- Main.py
+++ Main.py
@@ -5,15 +5,15 @@ from ipython_genutils.py3compat import execfile
 os.system('unzip ./Dataset/coco_annotation.zip -d ./Dataset')
 os.system('unzip ./annotations-test/image_info_test2014.zip -d ./annotations-test')
 
-os.system('pip install mask')
+os.system('pip3 install mask')
 
-os.system('python Create_dataset.py')
+os.system('python3 Create_dataset.py')
 
 os.system('git clone https://github.com/salesforce/ctrl.git')
 
 os.system('pip install fastBPE')
 
-os.system('cd ./ctrl/training_utils')
+os.chdir('./ctrl/training_utils')
 
 os.system('python ./make_tf_records.py --text_file ../../train.txt --control_code Caption --sequence_len 256')
 
@@ -21,7 +21,7 @@ os.system('gsutil -m cp -r gs://sf-ctrl/seqlen256_v1.ckpt/ .')
 
 os.system('python ./training.py --model_dir ./seqlen256_v1.ckpt --iterations 500')
 
-os.system('cd ..')
+os.chdir('..')
 
 os.system(' python ./generation.py --model_dir ./training_utils/seqlen256_v1.ckpt --temperature 0.4 --topk 4')
 
