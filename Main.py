a53a4e6ae3ee7a2fc21f320271cb6e677e571f0f
matteogally <s277935@d>
2021-08-12 12:47:16 +0200

    -minor fix

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
 import os
 
 #from ipython_genutils.py3compat import execfile
+os.system('pip install pycocotools')
+
+os.system('python Create_dataset.py')
 
 os.system('git clone https://github.com/salesforce/ctrl.git')
 
@@ -12,18 +15,11 @@ os.system('gsutil -m cp -r gs://sf-ctrl/seqlen256_v1.ckpt/ .')
 
 os.chdir('../..')
 
-os.system('unzip ./Dataset/coco_annotation.zip -d ./Dataset')
-os.system('unzip ./annotations-test/image_info_test2014.zip -d ./annotations-test')
-
 os.system('yes | conda create -n py27 python=2.7')
 
 os.system('conda activate py27')
 
-os.system('yes | conda install -c conda-forge pycocotools')
-
 os.system('yes | conda install matplotlib')
-
-os.system('python Create_dataset.py')
 
 os.system('yes | conda install -c conda-forge fastBPE')
 
@@ -43,9 +39,13 @@ os.system('yes | conda install tqdm')
 
 os.chdir('./ctrl/training_utils')
 
-os.system('python ./make_tf_records.py --text_file ../../train.txt --control_code caption --sequence_len 256')
+for i in range(4):
+    os.system('mv ../../train'+str(i)+'.txt .')
+    print('Make TFRecord of training file: '+str(i)+'\n')
+    os.system('python ./make_tf_records.py --text_file train'+str(i)+'.txt --control_code caption --sequence_len 256')
+    print('Finish TFRecord of training file: ' + str(i) + '\n')
 
-os.system('python ./training.py --model_dir ../seqlen256_v1.ckpt --iterations 500')
+os.system('python ./training.py --model_dir seqlen256_v1.ckpt --iterations 5')
 
 os.chdir('..')
 
