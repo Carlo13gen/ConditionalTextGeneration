71a50a2d09b5346cae1b55197f87e3fc420f96a1
LukasCanciani <48600448+LukasCanciani@users.noreply.github.com>
2021-08-22 15:50:32 +0200

    Added pos_bleu score function

--- Evaluation.py
+++ Evaluation.py
@@ -2,6 +2,7 @@ import sys
 
 from nltk.tokenize import RegexpTokenizer
 from nltk.translate import bleu_score
+from nltk import pos_tag
 import numpy as np
 
 splitter = RegexpTokenizer('\w+') #\w seleziona tutto cio che è un carattere "di parola"" . il + concatena alla selezione più "char parola" consecutivi
@@ -34,9 +35,16 @@ for l in gen_file:
 gen_file.close()
 print(gen)
 
+
+ref_pos = []
+for r in ref:
+    ref_pos.append(pos_tag(r))
+
 bleu = []
 self_bleu = []
 self_bleu_metodo2 = []
+pos_bleu = []
+  
 i = 0
 for g in gen:
     bleu.append(bleu_score.sentence_bleu(ref,g, smoothing_function=bleu_score.SmoothingFunction().method1))
@@ -46,11 +54,15 @@ for g in gen:
     gen_x = gen[:]
     gen_x.remove(g)
     self_bleu_metodo2.append(bleu_score.sentence_bleu(gen_x,g, smoothing_function=bleu_score.SmoothingFunction().method1))
+    
+    g_pos = pos_tag(g)
+    pos_bleu.append(bleu_score.sentence_bleu(ref_pos,g_pos, smoothing_function=bleu_score.SmoothingFunction().method1))
+    
     i=i+1
 
-print("Bleu score: %f\nSelf_Bleu score: %f(%f)"%(np.array(bleu).mean(), np.array(self_bleu).mean(), np.array(self_bleu_metodo2).mean()))
+print("Bleu score: %f\nSelf_Bleu score: %f(%f) Pos_bleu score:%f"%(np.array(bleu).mean(), np.array(self_bleu).mean(), np.array(self_bleu_metodo2).mean(), np.array(pos_bleu).mean()))
 
 if out_file != "":
     f_out = open(out_file, "a")
-    f_out.write("Bleu score: %f\nSelf_Bleu score: %f(%f)"%(np.array(bleu).mean(), np.array(self_bleu).mean(), np.array(self_bleu_metodo2).mean()))
+    f_out.write("Bleu score: %f\nSelf_Bleu score: %f(%f) Pos_bleu score:%f"%(np.array(bleu).mean(), np.array(self_bleu).mean(), np.array(self_bleu_metodo2).mean(), np.array(pos_bleu).mean()))
     f_out.close()
